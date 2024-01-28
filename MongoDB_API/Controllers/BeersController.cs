d7ecc05bcbc038c7a4ee2ef984110334f6ea5d0e
LIM YANG ZHE <21043383@myrp.edu.sg>
2024-01-28 12:51:08 +0800

    28 jan push

--- MongoDB_API/Controllers/BeersController.cs
+++ MongoDB_API/Controllers/BeersController.cs
@@ -19,19 +19,6 @@ namespace MongoDB_API.Controllers
         public async Task<List<Beers>> Get() =>
       await _beersService.GetAsync();
 
-        //[HttpGet]
-        //public async Task<List<Beers>> Get()
-        //{
-        //    //return await _beersService.GetAsync();
-        //    List<Beers> result = new List<Beers>();
-        //    result.Add(new Beers() {Id="655df74ea7e3f76d0a818073", DrugID = "b_t3_r8", Drug= "Chlorpromazine", 
-        //        DrugClass= "Antipsychotics", Crcl= "", Disease= "Syncope", Recommendation= "Avoid",
-        //        Rationale= "Increased risk of orthostatic hypotension",
-        //        StrengthRecommendation ="Weak", QualityEvidence= "High"});;
-        //    return result;
-        //}
-
-
 
         [HttpGet("{id:length(24)}")]
         public async Task<ActionResult<Beers>> Get(string id)
@@ -118,53 +105,7 @@ namespace MongoDB_API.Controllers
         }
 
 
-        //[HttpGet("recommendation/{id:length(24)}")]
-        //public async Task<ActionResult<string>> GetDrugRecommendation(string id)
-        //{
-        //    var beers = await _beersService.GetAsync(id);
-
-        //    if (beers is null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    // Assuming you have a property in your Beers class that contains the drug recommendation.
-        //    string drugRecommendation = beers.Recommendation;
-
-        //    return drugRecommendation;
-        //}
-
-
-        //[HttpGet("recommendation")]
-        //public async Task<ActionResult<List<string>>> GetDrugRecommendations([FromQuery] string ids)
-        //{
-        //    if (string.IsNullOrWhiteSpace(ids))
-        //    {
-        //        return BadRequest("Please provide a comma-separated list of drug IDs.");
-        //    }
-
-        //    var idList = ids.Split(',');
-
-        //    var recommendations = new List<string>();
-
-        //    foreach (var id in idList)
-        //    {
-        //        var beers = await _beersService.GetAsync(id);
-
-        //        if (beers != null)
-        //        {
-        //            recommendations.Add(beers.Recommendation);
-        //        }
-        //        else
-        //        {
-        //            recommendations.Add($"No recommendation found for drug with ID: {id}");
-        //        }
-        //    }
-
-        //    return recommendations;
-        //}
-
-
+     
 
     }
 }
